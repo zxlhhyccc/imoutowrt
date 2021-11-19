0faf63e4697b17c7bada4e0f99795203699e71f7
Robert Marko <robert.marko@sartura.hr>
2021-11-19 14:46:58 +0100

    kernel: package Qualcomm QMI Helpers as a module
    ath11k requires the Qualcomm QMI Helpers, so since the symbol is now
    selectable package it as kernel module so it can be added as a dependency
    in ath11k.
    
    Signed-off-by: Robert Marko <robert.marko@sartura.hr>

--- package/kernel/linux/modules/other.mk
+++ package/kernel/linux/modules/other.mk
@@ -1322,3 +1322,18 @@ define KernelPackage/mhi-pci-generic/description
 endef
 
 $(eval $(call KernelPackage,mhi-pci-generic))
+
+
+define KernelPackage/qcom-qmi-helpers
+  SUBMENU:=$(OTHER_MENU)
+  TITLE:=Qualcomm QMI Helpers
+  KCONFIG:=CONFIG_QCOM_QMI_HELPERS
+  FILES:=$(LINUX_DIR)/drivers/soc/qcom/qmi_helpers.ko
+  AUTOLOAD:=$(call AutoProbe,qmi_helpers)
+endef
+
+define KernelPackage/qcom-qmi-helpers/description
+  Qualcomm QMI Helpers
+endef
+
+$(eval $(call KernelPackage,qcom-qmi-helpers))
