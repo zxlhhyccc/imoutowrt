a7fe019e7152abe910d7061385f95dced28cc70e
MeIsReallyBa <38341209+MeIsReallyBa@users.noreply.github.com>
2022-04-11 22:10:04 +0800

    Update target.mk

--- target/linux/mediatek/mt7622/target.mk
+++ target/linux/mediatek/mt7622/target.mk
@@ -2,7 +2,7 @@ ARCH:=aarch64
 SUBTARGET:=mt7622
 BOARDNAME:=MT7622
 CPU_TYPE:=cortex-a53
-DEFAULT_PACKAGES += wpad-basic-wolfssl uboot-envtools swconfig bridger
+DEFAULT_PACKAGES += wpad-mesh-openssl uboot-envtools swconfig
 KERNELNAME:=Image dtbs
 
 define Target/Description
