efb3dd590f8b3c1d74605b273e9ae5d5e74588d1
zxlhhyccc <zxlhhyccc@gmail.com>
2022-10-22 13:58:55 +0800

    ipq807x: Update Qualcomm Atheros IPQ807x target

--- package/kernel/mac80211/files/lib/wifi/mac80211.sh
+++ package/kernel/mac80211/files/lib/wifi/mac80211.sh
@@ -196,19 +196,19 @@ detect_mac80211() {
 			set wireless.${name}=wifi-device
 			set wireless.${name}.type=mac80211
 			${dev_id}
-<			set wireless.${name}.channel=${channel}
+			set wireless.${name}.channel=${channel}
 			set wireless.${name}.band=${mode_band}
 			set wireless.${name}.htmode=$htmode
-			set wireless.${name}.country=US
 			set wireless.${name}.disabled=0
+			set wireless.${name}.country=US
 
 			set wireless.default_${name}=wifi-iface
 			set wireless.default_${name}.device=${name}
 			set wireless.default_${name}.network=lan
 			set wireless.default_${name}.mode=ap
-			set wireless.default_${name}.ssid=ImmortalWrt
-			set wireless.default_${name}.encryption=none
-			set wireless.default_${name}.iw_qos_map_set=none
+			set wireless.default_${name}.ssid=OpenWrt
+			set wireless.default_${name}.encryption=psk2
+			set wireless.default_${name}.key=1234567890
 EOF
 		uci -q commit wireless
 	done
--- target/linux/ipq807x/image/generic.mk
+++ target/linux/ipq807x/image/generic.mk
@@ -1,18 +1,18 @@
 define Device/FitImage
 	KERNEL_SUFFIX := -fit-uImage.itb
-	KERNEL = kernel-bin | gzip | fit gzip $$(KDIR)/image-$$(DEVICE_DTS).dtb
+	KERNEL = kernel-bin | gzip | fit gzip $$(DEVICE_DTS_DIR)/$$(DEVICE_DTS).dtb
 	KERNEL_NAME := Image
 endef
 
 define Device/FitImageLzma
 	KERNEL_SUFFIX := -fit-uImage.itb
-	KERNEL = kernel-bin | lzma | fit lzma $$(KDIR)/image-$$(DEVICE_DTS).dtb
+	KERNEL = kernel-bin | lzma | fit lzma $$(DEVICE_DTS_DIR)/$$(DEVICE_DTS).dtb
 	KERNEL_NAME := Image
 endef
 
 define Device/FitzImage
 	KERNEL_SUFFIX := -fit-zImage.itb
-	KERNEL = kernel-bin | fit none $$(KDIR)/image-$$(DEVICE_DTS).dtb
+	KERNEL = kernel-bin | fit none $$(DEVICE_DTS_DIR)/$$(DEVICE_DTS).dtb
 	KERNEL_NAME := zImage
 endef
 
@@ -78,6 +78,32 @@ define Device/qnap_301w
 endef
 TARGET_DEVICES += qnap_301w
 
+define Device/zte_mf269
+    $(call Device/FitImage)
+    $(call Device/UbiFit)
+    DEVICE_VENDOR := ZTE
+    DEVICE_MODEL := MF269
+    BLOCKSIZE := 128k
+    PAGESIZE := 2048
+    DEVICE_DTS_CONFIG := config@ac04
+    SOC := ipq8071
+    DEVICE_PACKAGES := ipq-wifi-zte_mf269 uboot-envtools kmod-usb3 kmod-usb-dwc3 kmod-usb-dwc3-qcom
+endef
+TARGET_DEVICES += zte_mf269
+
+define Device/tplink_xtr10890
+	$(call Device/FitImage)
+	$(call Device/UbiFit)
+	DEVICE_VENDOR := TPLINK
+	DEVICE_MODEL := XTR10890
+	BLOCKSIZE := 128k
+	PAGESIZE := 2048
+	DEVICE_DTS_CONFIG := config@hk01.c6
+	SOC := ipq8078
+	DEVICE_PACKAGES := ipq-wifi-tplink_xtr10890 uboot-envtools
+endef
+TARGET_DEVICES += tplink_xtr10890
+
 define Device/redmi_ax6
 	$(call Device/xiaomi_ax3600)
 	DEVICE_VENDOR := Redmi
@@ -85,6 +111,19 @@ define Device/redmi_ax6
 	DEVICE_PACKAGES := ipq-wifi-redmi_ax6
 endef
 TARGET_DEVICES += redmi_ax6
+
+define Device/tplink_xtr10890
+	$(call Device/FitImage)
+	$(call Device/UbiFit)
+	DEVICE_VENDOR := TPLINK
+	DEVICE_MODEL := XTR10890
+	BLOCKSIZE := 128k
+	PAGESIZE := 2048
+	DEVICE_DTS_CONFIG := config@hk01.c6
+	SOC := ipq8078
+	DEVICE_PACKAGES := ipq-wifi-tplink_xtr10890 uboot-envtools
+endef
+TARGET_DEVICES += tplink_xtr10890
 
 define Device/xiaomi_ax3600
 	$(call Device/FitImage)
@@ -108,20 +147,6 @@ define Device/xiaomi_ax9000
 	PAGESIZE := 2048
 	DEVICE_DTS_CONFIG := config@hk14
 	SOC := ipq8072
-	DEVICE_PACKAGES := ipq-wifi-xiaomi_ax9000 kmod-ath11k-pci ath11k-firmware-qcn9074 \
-	kmod-ath10k-ct ath10k-firmware-qca9887-ct
+	DEVICE_PACKAGES := ipq-wifi-xiaomi_ax9000 kmod-ath10k-ct ath10k-firmware-qca9887-ct
 endef
 TARGET_DEVICES += xiaomi_ax9000
-
-define Device/zte_mf269
-	$(call Device/FitImage)
-	$(call Device/UbiFit)
-	DEVICE_VENDOR := ZTE
-	DEVICE_MODEL := MF269
-	BLOCKSIZE := 128k
-	PAGESIZE := 2048
-	DEVICE_DTS_CONFIG := config@ac04
-	SOC := ipq8071
-	DEVICE_PACKAGES := ipq-wifi-zte_mf269 uboot-envtools
-endef
-TARGET_DEVICES += zte_mf269
